598daac0acba212bfa8c2b7842e4ff611f892cc9
KamDziok <kam_dziok@wp.pl>
2020-01-09 21:57:51 +0100

    poprawa baza

--- app/src/main/java/com/example/mojewydatki/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mojewydatki/ui/home/HomeFragment.kt
@@ -8,10 +8,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mojewydatki.MainActivity
 import com.example.mojewydatki.R
 import com.example.mojewydatki.ui.wydatek.WydatekFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 
 class HomeFragment : Fragment() {
@@ -35,15 +37,18 @@ class HomeFragment : Fragment() {
         recyclerView.adapter =HomeAdapter(db)
 
         //Obsluga FloatingActionButton (tego plusa) wyswietlenie
-        val fab = activity!!.fab as? FloatingActionButton
-        fab!!.visibility = View.VISIBLE
-        val fm = activity!!.supportFragmentManager
-        val fragment_wydatek = WydatekFragment()
+        val fab = activity!!.fab
+        fab!!.visibility = View.INVISIBLE
+//        val fm = activity!!.supportFragmentManager
+//        val fragment_wydatek = WydatekFragment()
         //aktywacja plusa przeładowanie fragmentu na WydatkiFragment i ukrycie plusa
-        fab!!.setOnClickListener(){
-            fm.beginTransaction().replace(R.id.nav_host_fragment,fragment_wydatek).commit()
-            fab!!.visibility = View.INVISIBLE
-        }
+//        fab.setOnClickListener(){
+//            //val home = fm.findFragmentByTag("nav_host_fragment")!!
+//            //if(home != null)
+////            fm.beginTransaction().remove(fm.findFragmentById(R.id.nav_host_fragment)!!.childFragmentManager.fragments.get(0)!!).commit()
+//            fab.visibility = View.INVISIBLE
+//            fm.beginTransaction().replace(R.id.nav_host_fragment, fragment_wydatek).commit()
+//        }
 
         return root
     }
