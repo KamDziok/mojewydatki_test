2c3ec8848880d76c3dafb454f80222090af70b16
ŁukszKwaśny <lukasz.kwasny@interia.pl>
2020-01-09 18:06:24 +0100

    drobne zmiany

--- app/src/main/java/com/example/mojewydatki/ui/kategorie/KategorieFragment.kt
+++ app/src/main/java/com/example/mojewydatki/ui/kategorie/KategorieFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.mojewydatki.R
+import com.example.mojewydatki.ui.home.PayDataBase
 import kotlinx.android.synthetic.main.fragment_konto.*
+import java.text.NumberFormat
 
 class KategorieFragment : Fragment() {
 
@@ -33,7 +36,19 @@ class KategorieFragment : Fragment() {
 
         dodaj_konto_button.setOnClickListener{
             var acount = categoryTitle_textedit.text.toString()
+            var saldo = konto_saldop_textedit.text.toString()
 
+            if(acount.isNotEmpty() && saldo.isNotEmpty()){
+                try {
+                    val db: PayDataBase = PayDataBase(this)
+                    val nf = NumberFormat.getInstance()
+                    val saldo = nf.parse(saldo).toDouble()
+                    db.dodajKonto(saldo, acount)
+                }catch (e: Exception) {
+                    var mesage = Toast.makeText(applicationContext, "Coś poszło nie tak", Toast.LENGTH_SHORT)
+                    mesage.show()
+                }
+            }
         }
         return root
     }
